811eea6f7196c401669b1ea4b31ff4e23ad46171
shruthikurva <kurvashruthi98@gmail.com>
2025-01-30 21:26:56 +0530

    Add files via upload

---
+++ DEMO.java
@@ -0,0 +1,22 @@
+public class DEMO {
+    public static void main(String[] args) {
+        byte byteVar=100;
+        System.out.println("byte value:"+byteVar);
+      
+        short shortVar=10000;
+        System.out.println("short value:"+shortVar);
+
+        int intVar=10000;
+        System.out.println("int value:"+intVar);
+         long longVar=100000000l;
+         System.out.println("long value:"+longVar);
+         float floatVar=10.5f;
+         System.out.println("float value:"+floatVar);
+         double doubleVar=10.5555;
+         System.out.println("double value:"+doubleVar);
+         char charVar='A';
+         System.out.println("char value:"+charVar);
+         boolean booleanVar=true;
+         System.out.println("boolean:"+booleanVar);
+    }
+}
